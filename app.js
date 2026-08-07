0e8b1922047b4101e14808133e3f379a7419cf85
Chocolate2269 <somtooo921@gmail.com>
2026-08-07 19:57:28 +0100

    Connected supabase to app. js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
-const SUPABASE_URL = "PASTE_YOUR_URL_HERE"
-const SUPABASE_ANON_KEY = "PASTE_YOUR_KEY_HERE"
+const SUPABASE_URL = "https://fjxkjowsjzjvnmqibnjr.supabase.co"
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZqeGtqb3dzanpqdm5tcWlibmpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYxMTUxNzMsImV4cCI6MjEwMTY5MTE3M30.Wai6MKZFDmvx9gIlvmUf_QVMcVJigKcAQtfqkJsG3Ms"
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
 async function signUp(){
