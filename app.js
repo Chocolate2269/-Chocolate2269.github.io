d87d1a55de596c31888e6cebd781f96401016505
Chocolate2269 <somtooo921@gmail.com>
2026-08-09 00:40:45 +0100

    Update Supabase URL and key in app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // 1. Connect to Supabase
-const supabaseUrl = 'https://chocolate2269.github.io/-Chocolate2269.github.io/'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZqeGtqb3dzanpqdm5tcWlibmpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYxMTUxNzMsImV4cCI6MjEwMTY5MTE3M30.Wai6MKZFDmvx9gIlvmUf_QVMcVJigKcAQtfqkJsG3Ms' // <-- you still need to get this 1 thing
-const supabase = supabase.createClient(supabaseUrl, supabaseKey)
+const supabaseUrl = 'https://fjxkjoswszjvnmqibnjr.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZqeGtqb3dzanpqdm5tcWlibmpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYxMTUxNzMsImV4cCI6MjEwMTY5MTE3M30.Wai6MKZFDmvx9gIlvmUf_QVMcVJigKcAQtfqkJsG3Ms'
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey)
 
 // 2. SIGN UP CODE
 document.getElementById('signupBtn').addEventListener('click', async () => {
